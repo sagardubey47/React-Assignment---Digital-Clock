b0cbd98d36d0e1c08d966469a31daab0012fc90a
sagardubey47 <sagardubey47@gmail.com>
2021-02-18 19:30:35 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,40 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
+
+     constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+     }
+
+    componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+    render() { 
 
         return(
             <>
-               
+               <h1 style={
+                   {padding: "10px", 
+                   backgroundColor: "yellow", 
+                   display:"inline-block",
+                   margin:"50px"
+                   }}>
+                   {this.state.date.toLocaleTimeString()}
+                   </h1>
             </>
         )
     }
